728890fb1353788c7f9c9b6df54a288b473cb61a
Aaron Santos <asantosabsalon@gmail.com>
2025-06-02 11:36:40 -0500

    feat: Personalizacion de los tabs

--- navegacion.js
+++ navegacion.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FontAwesome } from '@expo/vector-icons'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
@@ -11,9 +12,54 @@ import ScreenSetting from './screen/setting/ScreenSetting';
 function MyTabs() {
    return (
       <Tab.Navigator>
-         <Tab.Screen name="home" component={ScreenHome} options={{headerShown:false}}/>
-         <Tab.Screen name="about" component={ScreenAcercade}/>
-         <Tab.Screen name="ScreenSetting" component={ScreenSetting}/>
+         <Tab.Screen
+            name="Home" 
+            component={ScreenHome} 
+            options={{
+               title: 'menu',
+               headerShown:false,
+               tabBarIcon: ({color, size}) => <FontAwesome size={28} name='home' color={color}/>,
+               tabBarActiveTintColor: '#D5451B',
+               tabBarLabelPosition: 'beside-icon',
+               tabBarBadge: '67',
+               tabBarBadgeStyle: {
+                  color: '#222222',
+                  backgroundColor: '#1DCD9F',
+               }
+            }}
+         />
+
+         <Tab.Screen 
+            name="About" 
+            component={ScreenAcercade}
+            options={{
+               headerShown: false,
+               tabBarIcon: ({color, size}) => <FontAwesome size={28} name='info-circle' color={color}/>,
+               tabBarLabelPosition: 'beside-icon',
+               tabBarActiveTintColor: '#D5451B',
+               tabBarBadge: '1',
+               tabBarBadgeStyle: {
+                  color: 'white',
+                  backgroundColor: '#98ABEE',
+               }
+            }}
+         />
+
+         <Tab.Screen 
+            name="Settings" 
+            component={ScreenSetting}
+            options={{
+               headerShown: false,
+               tabBarIcon: ({color, size}) => <FontAwesome size={28} name='cog' color={color}/>,
+               tabBarLabelPosition: 'beside-icon',
+               tabBarActiveTintColor: '#D5451B',
+               tabBarBadge: '100',
+               tabBarBadgeStyle: {
+                  color: 'white',
+                  backgroundColor: '#DDA853',
+               }
+            }}
+         />
       </Tab.Navigator>
    )
 }
